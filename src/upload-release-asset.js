10b9b16be36ed7ae330bef40d6ff98e9e9f0d63b
AHQ Miness <ahqsecret@gmail.com>
2023-07-06 16:02:08 +0530

    new release

--- src/upload-release-asset.js
+++ src/upload-release-asset.js
@@ -1,14 +1,22 @@
 const core = require('@actions/core');
-const { GitHub } = require('@actions/github');
+const { context } = require('@actions/github');
+const gh = require('@actions/github');
 const fs = require('fs');
 
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = gh.getOctokit(process.env.GITHUB_TOKEN);
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const uploadUrl = core.getInput('upload_url', { required: true });
+    const id = core.getInput('id', { required: true });
+    
+    const uploadUrl = (await github.rest.repos.getRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      release_id: id
+    })).data.upload_url;
+
     const assetPath = core.getInput('asset_path', { required: true });
     const assetName = core.getInput('asset_name', { required: true });
     const assetContentType = core.getInput('asset_content_type', { required: true });
@@ -22,7 +30,7 @@ async function run() {
     // Upload a release asset
     // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
-    const uploadAssetResponse = await github.repos.uploadReleaseAsset({
+    const uploadAssetResponse = await github.rest.repos.uploadReleaseAsset({
       url: uploadUrl,
       headers,
       name: assetName,
